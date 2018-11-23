9d9869a97b4f57c7d072641ca2c4d9867692046e
Antonio Javier Rodriguez <ajrodriguez095@hotmail.com>
2018-11-24 00:53:13 +0100

    First update

---
+++ src/es/upm/dit/apsv/webLab/servlets/PublicationServlet.java
@@ -0,0 +1,29 @@
+package es.upm.dit.apsv.webLab.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import es.upm.dit.apsv.webLab.dao.PublicationDAOImplementation;
+import es.upm.dit.apsv.webLab.dao.ResearcherDAO;
+import es.upm.dit.apsv.webLab.dao.ResearcherDAOImplementation;
+import es.upm.dit.apsv.webLab.model.Publication;
+
+@WebServlet("/PublicationServlet")
+public class PublicationServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String publicationId = req.getParameter("id");
+		Publication publication = PublicationDAOImplementation.getInstance().read(publicationId);
+		req.getSession().setAttribute("publication", publication);
+		ResearcherDAO rdao = ResearcherDAOImplementation.getInstance();
+		req.getSession().setAttribute("firstAuthor", rdao.read(publication.getFirstAuthor()));
+		req.getSession().setAttribute("authors", rdao.parseResearchers(publication.getAuthors()));
+		getServletContext().getRequestDispatcher("/PublicationView.jsp").forward(req, resp);
+	}
+}
