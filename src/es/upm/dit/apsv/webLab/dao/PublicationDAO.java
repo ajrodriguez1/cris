3e230bf17f355504ec8f4bb1dd8f604086173964
Antonio Javier Rodriguez <ajrodriguez095@hotmail.com>
2018-11-24 15:27:16 +0000

    General updates

--- src/es/upm/dit/apsv/webLab/dao/PublicationDAO.java
+++ src/es/upm/dit/apsv/webLab/dao/PublicationDAO.java
@@ -1,13 +1,40 @@
 package es.upm.dit.apsv.webLab.dao;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import es.upm.dit.apsv.webLab.model.Publication;
+import es.upm.dit.apsv.webLab.dao.PublicationDAOImplementation;
 
 public class PublicationDAO {
+	PublicationDAOImplementation pdaoi;
+	
+	public PublicationDAO () {
+		this.pdaoi = PublicationDAOImplementation.getInstance();
+	}
+	
 	public Publication create (Publication publication) {
-		return new Publication();
+		return null;
 	}
 	
 	public Publication read (String publicationId) {
+		return null;
+	}
+	
+	public Publication update ( Publication publication ) {
+		return null;
+	}
+	
+	public Publication delete ( Publication publication ) {
+		return null;
+	}
+
+	public List<Publication> readAll (){
+		return null;
+	}
+	
+	public List<Publication> parsePublications (Collection<String> ids){
 		return null;
 	}
 }
--- src/es/upm/dit/apsv/webLab/dao/ResearcherDAO.java
+++ src/es/upm/dit/apsv/webLab/dao/ResearcherDAO.java
@@ -1,3 +1,48 @@
 package es.upm.dit.apsv.webLab.dao;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import es.upm.dit.apsv.webLab.model.Researcher;
+import es.upm.dit.apsv.webLab.dao.ResearcherDAOImplementation;
+
+public class ResearcherDAO{
+	ResearcherDAOImplementation rdaoi;
+	
+	public ResearcherDAO () {
+		this.rdaoi = ResearcherDAOImplementation.getInstance();
+	}
+	
+	public Researcher create (Researcher researcher) {
+		return rdaoi.create(researcher);
+	}
+	
+	public Researcher read (String researcherId) {
+		return null;
+	}
+	
+	public Researcher update (Researcher researcherId) {
+		return null;
+	}
+	
+	public Researcher delete (Researcher researcherId) {
+		return null;
+	}
+	
+	public List<Researcher> readAll (){
+		return null;
+	}
+	
+	public Researcher readAsUser (String email, String password) {
+		return null;
+	}
+	
+	public List<Researcher> parseResearchers (Collection<String> ids) {
+		return null;
+	}
+	
+	public Researcher parseResearchers (String researcherId) {
+		return null;
+	}
+}
